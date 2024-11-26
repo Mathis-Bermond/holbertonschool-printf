4bbc93a4f050f125477489d194daf058aa3197dc
Krosenvag <herve.leguennec13@gmail.com>
2024-11-26 12:46:28 +0000

    j'ai mis tous les fichiers en un seul du nom de get_func.c

---
+++ dif_func.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+        return (write(1, &c, 1));
+}
+#include "main.h"
+
+/**
+ * print_integer - Print a number in base 10
+ * @list: Number to print in base 10
+ *
+ * Return: Length of th numbers in decimal
+ **/
+int print_integer(va_list list)
+{
+	char *p_i;
+	int size;
+
+	p_i = itoa(va_arg(list, int), 10);
+
+	size = print((p_i != NULL) ? p_i : "NULL");
+
+	return (size);
+}
+#include "main.h"
+
+/**
+ * print_string - Print string
+ * @list: list.
+ *
+ * Return: String length.
+ */
+
+int print_string(va_list list)
+{
+	char *p;
+	int p_len;
+
+	p = va_arg(list, char*);
+	p_len = print((p != NULL) ? p : "(null)");
+
+	return (p_len);
+}
