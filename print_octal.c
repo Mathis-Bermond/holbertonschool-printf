8b717e1362e56eb174815e9942050e4c6e8c972d
Krosenvag <herve.leguennec13@gmail.com>
2024-11-27 09:42:28 +0000

    J'ai créer la fonction pour print octal mais quand j'essaye de le compiler il m'affiche deux erreur mais en tout cas pour betty il n'y as pas de probleme

---
+++ print_octal.c
@@ -0,0 +1,33 @@
+#include <stdarg.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_octal - Print an unsigned integer in octal base
+ * @list: List containing the number to print
+ *
+ * Return: Length of the printed number
+ **/
+int print_octal(va_list list)
+{
+	unsigned int num = va_arg(list, unsigned int);
+	char buffer[12]; /* Buffer large enough to hold octal representation */
+	int i = 0, size = 0;
+
+	if (num == 0)
+	{
+	write(1, "0", 1);
+	return (1);
+	}
+	while (num > 0)
+	{
+	buffer[i++] = (num % 8) + '0'; /* Get the last octal digit */
+	num /= 8; /* Remove the last octal digit */
+	}
+	while (i--)
+	{
+	write(1, &buffer[i], 1);
+	size++;
+	}
+
+	return (size);
+}
