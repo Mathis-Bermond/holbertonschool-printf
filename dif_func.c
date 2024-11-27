eec6f51c8cc98c2869ac62bf2451f1dfab4426cc
MBRorry <9985@holbertonstudents>
2024-11-27 15:03:53 +0100

    Updating the structure : adding new function to it

--- dif_func.c
+++ dif_func.c
@@ -2,27 +2,28 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
+* _putchar - writes the character c to stdout
+* @c: The character to print
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
 int _putchar(char c)
 {
-        return (write(1, &c, 1));
+		return (write(1, &c, 1));
 }
 #include "main.h"
 
 /**
- * print_integer - Print a number in base 10
- * @list: Number to print in base 10
- *
- * Return: Length of th numbers in decimal
- **/
+* print_integer - Print a number in base 10
+* @list: Number to print in base 10
+*
+* Return: Length of th numbers in decimal
+**/
 int print_integer(va_list list)
 {
 	char *p_i;
+
 	int size;
 
 	p_i = itoa(va_arg(list, int), 10);
@@ -34,15 +35,16 @@ int print_integer(va_list list)
 #include "main.h"
 
 /**
- * print_string - Print string
- * @list: list.
- *
- * Return: String length.
- */
+* print_string - Print string
+* @list: list.
+*
+* Return: String length.
+*/
 
 int print_string(va_list list)
 {
 	char *p;
+
 	int p_len;
 
 	p = va_arg(list, char*);
